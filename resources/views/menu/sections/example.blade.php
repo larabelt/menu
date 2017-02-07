96744d7d40091af816dd607cf946d54a59e5bcb5
Matt Crowe <github.mpc@zym.me>
2017-02-07 01:16:16 +0000

    init ohiocms menu package

--- resources/views/menu/sections/example.blade.php
+++ resources/views/menu/sections/example.blade.php
@@ -8,6 +8,6 @@ if ($section->param('active')) {
 <div class="section section-block {{ $section->param('class') }}">
     @include('ohio-content::section.sections._header')
     @include('ohio-content::section.sections._body')
-    @include('ohio-content::menu.web._show')
+    @include('ohio-menu::menu.web._show')
     @include('ohio-content::section.sections._footer')
 </div>
