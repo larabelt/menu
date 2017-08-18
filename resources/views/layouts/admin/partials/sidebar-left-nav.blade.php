14667b2eb7423a4bda2adba28a2c2a5fce132dce
Matt Crowe <github.mpc@zym.me>
2017-08-18 21:01:06 +0000

    extend teams functionality init

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -1,1 +1,3 @@
-<li><a href="/admin/belt/menu/menu-groups"><i class="fa fa-bars"></i> <span>Menus</span></a></li>
+@if(!$team)
+    <li><a href="/admin/belt/menu/menu-groups"><i class="fa fa-bars"></i> <span>Menus</span></a></li>
+@endif
