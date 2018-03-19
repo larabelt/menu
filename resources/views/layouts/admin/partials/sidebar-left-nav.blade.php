239939e140d5d7141ad5fdc35066c555bb3235a2
Matt Crowe <github.mpc@zym.me>
2018-03-19 14:39:07 +0000

    sidebar nav ids

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -1,3 +1,3 @@
 @if(Auth::user()->can('edit', Belt\Menu\MenuGroup::class))
-    <li><a href="/admin/belt/menu/menu-groups"><i class="fa fa-bars"></i> <span>Menus</span></a></li>
+    <li id="menu-admin-sidebar-left-menu-groups"><a href="/admin/belt/menu/menu-groups"><i class="fa fa-bars"></i> <span>Menus</span></a></li>
 @endif
