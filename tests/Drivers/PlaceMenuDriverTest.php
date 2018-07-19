66f0cab3d33b77c045d23f496d7813550ada8b80
Matt Crowe <github.mpc@zym.me>
2018-07-19 15:05:21 +0000

    tests. template tweaks

---
+++ tests/Drivers/PlaceMenuDriverTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Mockery as m;
+use Belt\Core\Testing\BeltTestCase;
+use Belt\Spot\Place;
+use Belt\Menu\Drivers\PlaceMenuDriver;
+use Belt\Menu\MenuHelper;
+use Belt\Menu\MenuItem;
+
+class PlaceMenuDriverTest extends BeltTestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * @covers \Belt\Menu\Drivers\PlaceMenuDriver::add
+     */
+    public function test()
+    {
+
+        $place = m::mock(Place::class);
+        $place->shouldReceive('getAttribute')->with('default_url')->andReturn('/places/some-place');
+
+        $menuItem = m::mock(MenuItem::class);
+        $menuItem->shouldReceive('getAttribute')->with('label')->andReturn('Some Place');
+        $menuItem->shouldReceive('getAttribute')->with('target')->andReturn(false);
+        $menuItem->shouldReceive('morphParam')->once()->with('places')->andReturn($place);
+
+        $adapter = new PlaceMenuDriver($menuItem, ['config' => []]);
+        $menuItem->adapter = $adapter;
+
+        # add
+        $menuHelper = m::mock(MenuHelper::class);
+        $menuHelper->shouldReceive('add')->once()->with('/places/some-place', 'Some Place', [], ['target' => 'default']);
+        $adapter->add($menuHelper);
+    }
+
+
+}
